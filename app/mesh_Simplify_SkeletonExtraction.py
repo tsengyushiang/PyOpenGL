db89bacd9e86e8b11fbe20ad19885a6ce69d47b2
yushinag <tsengyushiang@gmail.com>
2020-11-10 17:01:48 +0800

    add : mid point simplification

--- app/mesh_Simplify_SkeletonExtraction.py
+++ app/mesh_Simplify_SkeletonExtraction.py
@@ -30,6 +30,7 @@ ui.setupUi(MainWindow)
 scene = QtGLScene(ui.openGLWidget)
 MainWindow.show()
 
+# skeleton extration mesh
 uniformSkeleton = Uniform()
 uniformSkeleton.addvec3('viewPos', [0, 0, -1])
 uniformSkeleton.addMat4('normalizeMat', np.identity(4))
@@ -41,6 +42,7 @@ skeletonMesh = Mesh(matSkeleton, geoSkeleton)
 skeletonMesh.visible = False
 scene.add(skeletonMesh)
 
+# QEM simplification mesh
 uniformSimplify = Uniform()
 uniformSimplify.addvec3('viewPos', [0, 0, -1])
 uniformSimplify.addMat4('normalizeMat', np.identity(4))
@@ -49,8 +51,21 @@ matSimplify = ShaderMaterial(myShader.vertex_shader,
                      uniformSimplify)
 geoSimplify = OpenMeshGeometry()
 simplifyMesh = Mesh(matSimplify, geoSimplify)
-simplifyMesh.visible = True
+simplifyMesh.visible = False
 scene.add(simplifyMesh)
+
+# midpoint simplification
+uniformMidSimplify = Uniform()
+uniformMidSimplify.addvec3('viewPos', [0, 0, -1])
+uniformMidSimplify.addMat4('normalizeMat', np.identity(4))
+matMidSimplify = ShaderMaterial(myShader.vertex_shader,
+                     myShader.fragment_shader,
+                     uniformMidSimplify)
+geoMidSimplify = OpenMeshGeometry()
+midSimplifyMesh = Mesh(matMidSimplify, geoMidSimplify)
+midSimplifyMesh.visible = True
+scene.add(midSimplifyMesh)
+
 
 # setup UI
 def importObj():
@@ -65,6 +80,10 @@ def importObj():
     sucess = geoSimplify.readObj(filename)
     if sucess:
         uniformSimplify.setValue('normalizeMat',geoSimplify.getNormalizeMat())
+    
+    sucess = geoMidSimplify.readObj(filename)
+    if sucess:
+        uniformMidSimplify.setValue('normalizeMat',geoMidSimplify.getNormalizeMat())
 
     scene.endDraw()
 ui.pushButton_2.clicked.connect(importObj)
@@ -82,9 +101,17 @@ def enableSimplify():
     updateSimplify = True
 ui.pushButton_3.clicked.connect(enableSimplify)
 
+updateMidSimplify = False
+def enableMidSimplify():
+    global updateMidSimplify
+    updateMidSimplify = True
+ui.pushButton.clicked.connect(enableMidSimplify)
+
 def onHandleSkeletonSlider(value):
     skeletonMesh.visible = True
     simplifyMesh.visible = False
+    midSimplifyMesh.visible = False
+
 
     range = ui.horizontalScrollBar_2.maximum()-ui.horizontalScrollBar_2.minimum()
     geoSkeleton.setLevel(float(value)/range)        
@@ -93,14 +120,25 @@ ui.horizontalScrollBar_2.valueChanged.connect(onHandleSkeletonSlider)
 def onHandleSimplifySlider(value):
     skeletonMesh.visible = False
     simplifyMesh.visible = True
+    midSimplifyMesh.visible = False
 
     range = ui.horizontalScrollBar_3.maximum()-ui.horizontalScrollBar_3.minimum()
     geoSimplify.setLevel(float(value)/range)        
 ui.horizontalScrollBar_3.valueChanged.connect(onHandleSimplifySlider)
 
+def onHandleMidSimplifySlider(value):
+    skeletonMesh.visible = False
+    simplifyMesh.visible = False
+    midSimplifyMesh.visible = True
+
+    range = ui.horizontalScrollBar.maximum()-ui.horizontalScrollBar.minimum()
+    geoMidSimplify.setLevel(float(value)/range)        
+ui.horizontalScrollBar.valueChanged.connect(onHandleMidSimplifySlider)
+
+
 # render
 def mainloop():
-    global updateSkeleton,updateSimplify
+    global updateSkeleton,updateSimplify,updateMidSimplify
     uniformSkeleton.setValue('viewPos', [
         scene.camera.position[0],
         scene.camera.position[1],
@@ -109,7 +147,13 @@ def mainloop():
     uniformSimplify.setValue('viewPos', [
         scene.camera.position[0],
         scene.camera.position[1],
-        scene.camera.position[2]])    
+        scene.camera.position[2]])
+    
+    uniformMidSimplify.setValue('viewPos', [
+        scene.camera.position[0],
+        scene.camera.position[1],
+        scene.camera.position[2]]) 
+
     scene.startDraw()
 
 
@@ -127,6 +171,14 @@ def mainloop():
             geoSimplify.init()
         except:
             updateSimplify = False
+    
+    if updateMidSimplify:
+        try:
+            print('simplify')
+            updateMidSimplify = geoMidSimplify.collapseFirstEdge()
+            geoMidSimplify.init()
+        except:
+            updateMidSimplify = False
 
     scene.endDraw()
 
